3d4426cb7ccc780bf06a64ccd97a78664327621e
Dixanta Bahadur Shrestha <dixanta@gmail.com>
2019-01-13 23:19:44 +0545

    Create naive_algorithm.js

---
+++ naive_algorithm.js
@@ -0,0 +1,25 @@
+var txt="abcabcdcabcabababaabc";
+var pat="abc";
+
+function naive(txt,pat){
+    txt=txt.toLowerCase();
+    pat=pat.toLowerCase();
+    var txtLength=txt.length;
+    var patLength=pat.length;
+
+    var counter=0;
+    for(var i=0;i<=(txtLength-patLength);i++){
+        //console.log(txt.charAt(i));
+        var total=0;
+        for(var j=0;j<patLength;j++){
+            if(txt.charAt(i+j)==pat.charAt(j)){
+                total++;
+            }
+        }
+        if(total==patLength){
+            counter++;
+        }
+    }
+    return counter;
+}
+console.log(naive(txt,pat));
