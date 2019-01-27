6ee296f79c19d18ee8ff9ff11a88d39952e07d40
Dixanta Bahadur Shrestha <dixanta@gmail.com>
2019-01-27 08:48:00 +0545

    Create App.java

---
+++ java/screencapture/App.java
@@ -0,0 +1,28 @@
+import java.awt.Color;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class App{
+    public static void main(String[] args)throws Exception{
+        System.out.println("Capturing start");
+        int i=0;
+        File file=new File("capture");
+        if(!file.exists() || !file.isDirectory()){
+            file.mkdir();
+        }
+        while(true){
+            i++;
+            Thread thread=new Thread(new CaptureThread(i));
+            thread.start();
+            Thread.sleep(5000);
+            
+            
+        }
+    }
+}
