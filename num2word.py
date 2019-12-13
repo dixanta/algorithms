75c1871f5ce807285efb2828eeea0716472cd481
Dixanta Bahadur Shrestha <dixanta@gmail.com>
2019-12-13 20:36:54 +0545

    Update num2word.py

--- num2word.py
+++ num2word.py
@@ -24,14 +24,25 @@ def two_places(num_str):
     return ""
 
 
-num=9945
-num_str=str(num)
-num_length=len(num_str)
-if(num_length==1):
-    print(numbers[int(num_str)])
-if(num_length==2):
-    print(two_places(num_str))
-elif(num_length==3):
-    print(numbers[int(num_str[0])] + " hundred " + two_places(num_str[1:]))
-elif(num_length==4):
-    print(numbers[int(num_str[0])] + " thousand " +numbers[int(num_str[1])] + " hundred " + two_places(num_str[2:]))
+def num2word(num):
+    num_str=str(num)
+    num_length=len(num_str)
+    if(num_length==1):
+        print(numbers[int(num_str)])
+    if(num_length==2):
+        print(two_places(num_str))
+    elif(num_length==3):
+        print(numbers[int(num_str[0])] + " hundred " + two_places(num_str[1:]))
+    elif(num_length==4):
+        print(numbers[int(num_str[0])] + " thousand " +numbers[int(num_str[1])] + " hundred " + two_places(num_str[2:]))
+
+
+num2word(0)
+num2word(10)
+num2word(15)
+num2word(20)
+num2word(45)
+num2word(165)
+num2word(555)
+num2word(9999)
+
