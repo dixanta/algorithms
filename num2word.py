331c3b7cd4ffa13f8e116ef9a75ba0ea69aba14d
Dixanta Bahadur Shrestha <dixanta@gmail.com>
2019-12-14 19:02:33 +0545

    removed 0 values in the middle
    converted 100 to hundred only
    converted 101  to one hundred one etc

--- num2word.py
+++ num2word.py
@@ -11,18 +11,22 @@ less_20=["Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen",
 "Sixteen","Seventeen","Eighteen","Nineteen"]
 
 below_hundred=["Twenty","thirty",
-"fourty","fifty","sixty","seventy","eighty","ninty"]
+"fourty","fifty","sixty","seventy","eighty","ninety"]
 
 def two_places(num_str):
+   
     num=int(num_str)
-    if(num<20):
+    if(num<10):
+        if(num==0):
+            return ''
+        else: return numbers[int(num_str)].capitalize()
+    elif(num >=10 and num<20):
         return less_20[int(num_str[1])].capitalize()
-    elif(num>=20):
-        if(int(num_str[1])==0):
-           return (below_hundred[int(num_str[0])-2]).capitalize()
-        else:
-            return(below_hundred[int(num_str[0])-2].capitalize() +" " + numbers[int(num_str[1])]).capitalize()    
-    return ""
+    else:
+        result=below_hundred[int(num_str[0])-2].capitalize() 
+        if (int(num_str[1])!=0): result += " " + numbers[int(num_str[1])].capitalize()
+        return result
+    
 
 
 def num2word(num):
@@ -35,11 +39,16 @@ def num2word(num):
     elif(num_length==3):
         print(numbers[int(num_str[0])] + " Hundred " + two_places(num_str[1:]))
     elif(num_length==4):
-        print(numbers[int(num_str[0])] + " Thousand " +numbers[int(num_str[1])] + " Hundred " + two_places(num_str[2:]))
+        pos=int(num_str[1])
+        value=''
+        if(pos > 0): value = numbers[pos] + " Hundred "
+
+        print(numbers[int(num_str[0])] + " Thousand " + value + two_places(num_str[2:]))
     elif(num_length==5):
-        print(two_places(num_str[0:2]) + " Thousand " +numbers[int(num_str[1])] + " Hundred " + two_places(num_str[3:]))
-
-
+        pos=int(num_str[2])
+        value=''
+        if(pos > 0): value = numbers[pos] + " Hundred "
+        print(two_places(num_str[0:2]) + " Thousand " + value + two_places(num_str[3:]))
 
 
 num2word(0)
@@ -47,8 +56,13 @@ num2word(10)
 num2word(15)
 num2word(20)
 num2word(45)
+num2word(100)
 num2word(165)
 num2word(555)
+num2word(1001)
+num2word(1010)
 num2word(9999)
+num2word(10001)
+num2word(10010)
 num2word(99989)
 
